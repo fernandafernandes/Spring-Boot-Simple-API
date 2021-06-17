3b908efa95d62de1aa0a08c6a4da326470688463
Fernanda <ferffmm@gmail.com>
2021-06-17 15:53:22 -0300

    Criado repo app spring boot

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,9 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+}
